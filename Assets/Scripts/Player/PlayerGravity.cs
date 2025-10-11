b82cf3dcdaeba7f6b5383dcc36ff1f73a8ccadff
Jordan Baumann <69920353+Cat-Brain@users.noreply.github.com>
2025-10-11 18:24:30 -0400

    Improved tap jumps and added crumble blocks

--- Assets/Scripts/Player/PlayerGravity.cs
+++ Assets/Scripts/Player/PlayerGravity.cs
@@ -4,6 +4,8 @@ public class PlayerGravity : MonoBehaviour
 {
     public float gravity, tapJumpGravity;
 
+    public bool inQuickFall = false;
+
     private PlayerManager playerManager;
     private Rigidbody2D rb;
 
@@ -15,7 +17,9 @@ public class PlayerGravity : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.linearVelocityY -= (rb.linearVelocityY <= 0 || playerManager.Stunned() ||
-            playerManager.jumpAction.action.inProgress ? gravity : tapJumpGravity) * Time.fixedDeltaTime;
+        if (inQuickFall && (rb.linearVelocityY <= 0 || playerManager.Stunned() ||
+            playerManager.jumpAction.action.inProgress))
+            inQuickFall = false;
+        rb.linearVelocityY -= (inQuickFall ? tapJumpGravity : gravity) * Time.fixedDeltaTime;
     }
 }
--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -14,6 +14,7 @@ public class PlayerMove : MonoBehaviour
     [Tooltip("Time that a player cannot jump after having done a jump (removes buggy unfun tech)")]
     public float jumpSpamTime;
     private float jumpBufferTimer = 0, cayoteTimer = 0, jumpSpamTimer = 0;
+    public float tapJumpTimer = 0;
 
     public PhysicsMaterial2D movePhysicsMaterial, stunnedPhysicsMaterial;
 
@@ -26,12 +27,19 @@ public class PlayerMove : MonoBehaviour
         playerManager = GetComponent<PlayerManager>();
 
         playerManager.jumpAction.action.started += TryJump;
+        playerManager.jumpAction.action.canceled += TryTapJump;
     }
 
     public void TryJump(InputAction.CallbackContext context)
     {
         if (!playerManager.Stunned())
             jumpBufferTimer = jumpBufferTime;
+    }
+
+    public void TryTapJump(InputAction.CallbackContext context)
+    {
+        if (tapJumpTimer > 0)
+            playerManager.playerGravity.inQuickFall = true;
     }
 
     private void FixedUpdate()
@@ -61,6 +69,7 @@ public class PlayerMove : MonoBehaviour
                 jumpBufferTimer = 0;
                 cayoteTimer = 0;
                 jumpSpamTimer = jumpSpamTime;
+                tapJumpTimer = jumpForce / playerManager.playerGravity.gravity;
             }
         }
         else
@@ -69,5 +78,8 @@ public class PlayerMove : MonoBehaviour
         jumpBufferTimer = Mathf.Max(0, jumpBufferTimer - Time.fixedDeltaTime);
         cayoteTimer = Mathf.Max(0, cayoteTimer - Time.fixedDeltaTime);
         jumpSpamTimer = Mathf.Max(0, jumpSpamTimer - Time.fixedDeltaTime);
+        tapJumpTimer = Mathf.Max(0, tapJumpTimer - Time.fixedDeltaTime);
+        if (rb.linearVelocityY <= 0)
+            tapJumpTimer = 0;
     }
 }
