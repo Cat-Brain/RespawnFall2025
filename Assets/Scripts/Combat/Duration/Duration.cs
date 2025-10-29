1b7b01953ed7cb63e3b40f39644726de0945bf37
Jordan Baumann <0501jordanb@gmail.com>
2025-10-29 07:49:12 -0400

    Unfinished start of status effects and new hit system

---
+++ Assets/Scripts/Combat/Duration/Duration.cs
@@ -0,0 +1,5 @@
+public abstract class Duration
+{
+    // Returns true if this StatusEffect should be removed
+    public abstract bool Update(StatusEffect statusEffect);
+}
---
+++ Assets/Scripts/Combat/Hit.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Hit
+{
+    public float damage;
+
+    //public Hit(float damage, List)
+}
