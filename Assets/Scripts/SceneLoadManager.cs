bce2e399186f11cafcf32bf0b95d64614377baf1
Jordan Baumann <0501jordanb@gmail.com>
2025-11-11 18:52:33 -0500

    New Menu System
    Will probably break some other branch's code, sorry about that =[

--- Assets/Scripts/SceneLoadManager.cs
+++ Assets/Scripts/SceneLoadManager.cs
@@ -9,6 +9,7 @@ using UnityEngine.UI;
 public class SceneLoadManager : MonoBehaviour
 {
     public Image fadeBox;
+    public string sceneToLoad;
 
     public float fadeTime;
 
@@ -23,21 +24,26 @@ public class SceneLoadManager : MonoBehaviour
         }
     }
 
-    public void StartLoad(string sceneName, UnityEvent onBlackScreen = null)
+    public void SetSceneToLoad(string sceneToLoad)
+    {
+        this.sceneToLoad = sceneToLoad;
+    }
+
+    public void StartLoad(UnityEvent onBlackScreen = null)
     {
         if (loadingScene)
             return;
 
         loadingScene = true;
-        StartCoroutine(LoadSceneInternal(sceneName, onBlackScreen));
+        StartCoroutine(LoadSceneInternal(onBlackScreen));
     }
 
-    public IEnumerator LoadSceneInternal(string sceneName, UnityEvent onBlackScreen = null)
+    public IEnumerator LoadSceneInternal(UnityEvent onBlackScreen = null)
     {
         fadeBox.enabled = true;
         fadeBox.DOFade(1, fadeTime);
 
-        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName);
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneToLoad);
         asyncLoad.allowSceneActivation = false;
 
         yield return new WaitForSeconds(fadeTime);
