4c7af7a7e19298a5dae4e2c7631251ce4a23d76f
NeuvieParakeet <cmendez5063@floridapoly.edu>
2025-12-05 15:17:32 -0500

    All buttons have click sound

--- Assets/Scripts/Player/SFXManager.cs
+++ Assets/Scripts/Player/SFXManager.cs
@@ -3,23 +3,21 @@ using FMODUnity;
 
 public class SFXManager : MonoBehaviour
 {
+    public static SFXManager sfxManager;
     public EventReference sfxEvent;
 
     private FMOD.Studio.EventInstance instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
 
     public void PlayClick()
     {
         RuntimeManager.PlayOneShot(sfxEvent);
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
