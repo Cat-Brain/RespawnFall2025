8d1bc3c428854f4f7b13816f4229b7d3d3fb7d24
Jordan Baumann <0501jordanb@gmail.com>
2025-12-06 00:42:20 -0500

    So much LDTK stuff

--- Assets/Scripts/Tick/RestockTick.cs
+++ Assets/Scripts/Tick/RestockTick.cs
@@ -1,13 +1,10 @@
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RestockTick : OnTickEffect
 {
-    public List<ShopItem> items;
-
     public override void OnTick(TickEntity tickEntity)
     {
-        foreach (ShopItem item in items)
+        foreach (ShopItem item in FindObjectsByType<ShopItem>(FindObjectsSortMode.None))
             item.Restock();
     }
 }
