5f96ec85ae59b9d8ca7997346770f4fb3e2608cd
NeuvieParakeet <cmendez5063@floridapoly.edu>
2025-12-05 23:58:52 -0500

    Added a bunch of sounds

--- Assets/Scripts/Player/SFXManager.cs
+++ Assets/Scripts/Player/SFXManager.cs
@@ -6,7 +6,7 @@ public class SFXManager : MonoBehaviour
     public static SFXManager Instance;
     
 
-    public EventReference click, shootFire, shootIce, hitIce, statusIce, jump;
+    public EventReference click, shootFire, shootIce, hitIce, statusIce, hitFire, statusFire, jump;
 
     private FMOD.Studio.EventInstance instance;
 
